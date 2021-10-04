afc9510120c71cb3307951bd09c7565939f6405a
michmash2010 <michellecrowe2010@gmail.com>
2021-10-04 03:08:07 -0400

    add fetched data for current weather and dynamic h2 text plus icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,10 @@ var yyyy = today.getFullYear();
 var today = "(" + mm + "/" + dd + "/" + yyyy + ")";
 var cityDateHeadingEl = document.querySelector("#city-date-heading");
 var savedCityButtonsEl = document.querySelector("#saved-city-buttons");
-
+var todayTempEl = document.querySelector("#today-temp");
+var todayWindEl = document.querySelector("#today-wind");
+var todayHumidityEl = document.querySelector("#today-humidity");
+var todayFeelsLikeEl = document.querySelector("#today-feels-like");
 
 var formSubmitHandler = function(event) {
     //prevent page from refereshing
@@ -23,8 +26,10 @@ var formSubmitHandler = function(event) {
         
         // clear old content
         cityInputEl.textContent = "";
-        createCityButtons(searchCity);
+        
         // add the new city as a button
+        createCityButtons(searchCity);
+        
 
     } else {
         alert("Please enter a valid city name.");
@@ -38,15 +43,14 @@ var createCityButtons = function(searchCity) {
     cityButtonEl.textContent = searchCity;
     cityButtonEl.style.background = "grey";
     cityButtonEl.style.margin = "15px 0 15px 0";
+    cityButtonEl.style.color = "black";
 
     savedCityButtonsEl.appendChild(cityButtonEl);
 }
 
 var date = function(today) {
     console.log(today);
-
-    cityDateHeadingEl.textContent += today;        
-
+    
 }
 
 var displayTodayForecast = function() {
@@ -62,7 +66,7 @@ var displayFiveDayForecast = function() {
 var getWeatherData = function(searchCity) {
     console.log("made it to getWeatherData function");
     // api call to OpenWeather API
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=285b993a17ae7fb8fb9d5126a5cb88f5";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&units=imperial&appid=285b993a17ae7fb8fb9d5126a5cb88f5";
 
     fetch(apiUrl)
         .then(function(response) {
@@ -70,9 +74,45 @@ var getWeatherData = function(searchCity) {
                 response.json()
                     .then(function(data) {
                         console.log("inside fetch response");
-                        console.log(data)
+                        console.log(data);
+
+                        // display Current Temp
+                        console.log(data.main.temp);
+                        todayTempEl.textContent = data.main.temp + "°F";
+
+                        // display Current Wind Speed
+                        console.log(data.wind.speed);
+                        todayWindEl.textContent = data.wind.speed + " MPH";
+                        
+                        // display Current Humidity
+                        console.log(data.main.humidity);
+                        todayHumidityEl.textContent = data.main.humidity + " %";
+
+                        // display Current Feels-Like Temp
+                        console.log(data.main.feels_like);
+                        todayFeelsLikeEl.textContent = data.main.feels_like + "°F";
+
+                        // add the selected city to h2
+                        cityDateHeadingEl.textContent = searchCity;
+
+                        // add the date to h2
+                        cityDateHeadingEl.textContent += " " + today;
+
+                        // create the icon on h2
+                        console.log(data.weather[0].icon);
+                        var iconEl = document.createElement("span");
+                        iconEl.innerHTML = 
+                            // `<img src="http://openweathermap.org/img/wn/10d@` + data.weather[0].icon + `.png" />`;
+                            `<img src="http://openweathermap.org/img/wn/01n.png" />`;
+                        cityDateHeadingEl.appendChild(iconEl);
+
+                        // display Icon
+                        //cityDateHeadingEl.innerHTML = "<img src="http://openweathermap.org/img/wn/10d@" + data.weather[0].icon + ".png /></span>";
+                        //http://openweathermap.org/img/wn/10d@2x.png
+                        //console.log(data[0].coord.id);
                         displayTodayForecast();
                         displayFiveDayForecast();
+
                     });
             } else {
                 alert("Error: City Not Found" + response.statusTest);
